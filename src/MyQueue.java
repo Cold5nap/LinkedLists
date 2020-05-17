6f4db913dce65d55203e4a6012c7d837fe86a30b
Cold5nap <and.andr2013@yandex.ru>
2020-05-17 03:09:10 +0300

    task 2 #27

--- src/MyQueue.java
+++
@@ -1,6 +0,0 @@
-public interface MyQueue<T> {
-    void enqueue(T value);
-    T dequeue() throws Exception;
-    T peek() throws Exception;
-    boolean empty();
-}
--- src/MyStack.java
+++
@@ -1,6 +0,0 @@
-public interface MyStack<T> {
-    void push(T value);
-    T peek() throws Exception;
-    T pop() throws Exception;
-    boolean empty();
-}
